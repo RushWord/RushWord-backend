65c4484e4bd1d79c528a1adf4e75169b7ede25b6
Maher <itsabomalek@icloud.com>
2025-07-09 16:49:06 +0200

    task 6 and 8 is done

--- src/Wordle.java
+++ src/Wordle.java
@@ -4,20 +4,40 @@ import java.util.List;
 public class Wordle {
     public static void main(String[] args) {
         System.out.println("Welcome to Wordle");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose language\n1 English\n2 Spanish");
+        String language = "";
+    while(!language.equalsIgnoreCase("English") && !language.equalsIgnoreCase("Spanish")){
 
-        List<String> words = WordLoader.loadWordsFromFile("dictionary.txt");
+        language = input.nextLine();
+        if(!language.equalsIgnoreCase("English") && !language.equalsIgnoreCase("Spanish")){
+            System.out.println("Invalid input");
+        }
+    }
+
+
+
+        List<String> words = WordLoader.loadWordsFromFile(language +".txt");
 
         WordGuessGame myGame = new WordGuessGame(words.toArray(new String[0]));
-        Scanner input = new Scanner(System.in);
-        System.out.println(myGame.chosenWord); //  << Just for test
-
+        System.out.println("(Debug) Word to guess: " + myGame.chosenWord); //  << Just for test
         while (true) {
-            String userGuess = input.nextLine();
-            if (myGame.userGuess(userGuess)) {
-                System.out.println("Correct");
-                break;
-            } else {
-                System.out.println("It's not correct");
+            try{
+                 String userGuess = input.nextLine().trim();
+
+                List<String> result = myGame.userGuess(userGuess);
+                System.out.println(result);
+                 if (!result.contains("red")) {
+                     System.out.println("Correct");
+                     break;
+                 } else {
+                     System.out.println("try again");
+
+                 }
+
+
+            } catch (Exception e) {
+                System.out.println(e);
             }
         }
     }
